e4dacab3aeeb347891a4b5927fd9ad443a57a8ac
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-05-24 13:20:48 +0100

    add examples to `Tactics.lean` that the tactics should be able to solve

--- LeanGeom/Tactics.lean
+++ LeanGeom/Tactics.lean
@@ -1,6 +1,7 @@
 import Mathlib.Geometry.Euclidean.Angle.Oriented.Basic
 
 notation "∠" A:max B:max => Complex.orientation.oangle A B
+instance : Fact <| Module.finrank ℝ ℂ = 2 := ⟨Complex.finrank_real_complex⟩
 
 
 -- #check  AddCircle.equivAddCircle
@@ -15,3 +16,28 @@ And finally, call `abel`
 /-
 For distances with multiplication, use `log` to cast it into something additive, and a positivity side goal.
 -/
+
+section Angles
+
+elab "abel_angle" : tactic => return
+
+-- example : (↑(Real.pi/2) : Real.Angle) = ↑((5/2) * Real.pi) := by
+--   abel_angle
+
+-- example (A B : ℂ) : ∠ A B = ∠ B A + ↑(9 * Real.pi) := by
+--   abel_angle
+
+end Angles
+
+
+section Distance
+
+-- example (A B : ℂ) (h : A ≠ B) : dist A B ≠ 0 := by
+--   positivity
+
+elab "mul_abel" : tactic => return
+
+-- example (x y : ℝ) (h₁ : x ≠ 0) (h₂ : y ≠ 0) : x^2 * y⁻¹ * x⁻¹^2 * y = 1 := by
+--   mul_abel
+
+end Distance
