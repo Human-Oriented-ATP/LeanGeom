7c8073e5ca22eabe61586e8cef0e69e68426c297
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-05-24 22:44:00 +0100

    feat: abel_angle

--- LeanGeom/Tactics.lean
+++ LeanGeom/Tactics.lean
@@ -1,6 +1,9 @@
 import Mathlib.Geometry.Euclidean.Angle.Oriented.Basic
+import Mathlib.Tactic.NormNum
 import Lean
 import Qq
+
+namespace Tactic
 
 open Lean Elab Meta Qq
 
@@ -9,8 +12,6 @@ instance : Fact <| Module.finrank ℝ ℂ = 2 := ⟨Complex.finrank_real_complex
 noncomputable def RayAngle (A B : ℂ) : Real.Angle := Complex.orientation.oangle (A - B) 1
 
 notation "∠" A:max B:max => RayAngle A B
-
--- #check  AddCircle.equivAddCircle
 
 /-
 first use simp with `equivAddCircle` to cast into `AddCircle 1`, and push the cast.
@@ -33,27 +34,93 @@ theorem rayAngle_swap (A B : ℂ) (h : A ≠ B) : ∠ B A = ∠ A B + π := by
   unfold RayAngle
   rw [← Orientation.oangle_neg_left _ (sub_ne_zero_of_ne h) (by norm_num), neg_sub]
 
-simproc raySwap (RayAngle _ _) := .ofQ fun
+simproc_decl raySwap (RayAngle _ _) := .ofQ fun
   | 1, ~q(Real.Angle), ~q(∠ $B $A) => do
     if Expr.lt A B then
-      let some e ← dischargeUsingAssumption? q($A ≠ $B) | return .continue
+      let some e ← dischargeUsingAssumption? q($A ≠ $B) | throwError "couldn't find a proof of {q($A ≠ $B)} in the local context"
       let some e ← checkTypeQ e q($A ≠ $B) | return .continue
       return .done <| .mk q(∠ $A $B + π) (some q(rayAngle_swap $A $B $e))
     else
       return .continue
 
 
-elab "abel_angle" : tactic => return
+private noncomputable def AngleEquiv : Angle ≃+ AddCircle (1 : ℝ) :=
+  AddCircle.equivAddCircle (2 * π) 1 (by positivity) (by positivity)
 
--- example : (↑(Real.pi/2) : Real.Angle) = ↑((5/2) * Real.pi) := by
---   abel_angle
+private lemma angleEquiv_inj {x y : Angle} : AngleEquiv x = AngleEquiv y → x = y := by apply AddEquiv.injective
 
--- example (A B : ℂ) : ∠ A B = ∠ B A + ↑(9 * Real.pi) := by
---   abel_angle
+private lemma map_angleEquiv_zero : AngleEquiv 0 = 0 := map_zero AngleEquiv
+private lemma map_angleEquiv_neg (x : Angle) : AngleEquiv (-x) = -AngleEquiv x := map_neg AngleEquiv x
+private lemma map_angleEquiv_nsmul (n : Nat) (x : Angle) : AngleEquiv (n • x) = n • AngleEquiv x := map_nsmul AngleEquiv n x
+private lemma map_angleEquiv_add (x y : Angle) : AngleEquiv (x + y) = AngleEquiv x + AngleEquiv y := map_add AngleEquiv x y
+private lemma map_angleEquiv_sub (x y : Angle) : AngleEquiv (x - y) = AngleEquiv x - AngleEquiv y := map_sub AngleEquiv x y
 
-example (A B : ℂ) (h : A ≠ B) : ∠ A B = ∠ B A - π := by
-  simp only [raySwap, add_sub_cancel_right]
+private lemma angleEquiv_pi : AngleEquiv π = ↑(1/2 : Real) := by
+  unfold AngleEquiv Angle.coe Angle
+  rw [AddCircle.equivAddCircle_apply_mk]
+  simp [pi_ne_zero]
 
+private lemma angleEquiv_mul_pi (x : ℝ) : AngleEquiv ↑(x * π) = ↑(1/2 * x : Real) := by
+  unfold AngleEquiv Angle.coe Angle
+  rw [AddCircle.equivAddCircle_apply_mk, mul_assoc]
+  simp [pi_ne_zero]
+  ring_nf
+
+private lemma angleEquiv_pi_div (x : ℝ) : AngleEquiv ↑(π / x) = ↑((2 * x)⁻¹ : Real) := by
+  unfold AngleEquiv Angle.coe Angle
+  rw [AddCircle.equivAddCircle_apply_mk, div_mul_eq_mul_div]
+  simp [pi_ne_zero]
+  ring_nf
+
+macro "angle_preprocess" : tactic => `(tactic| (
+  refine angleEquiv_inj ?_
+  simp only [
+    map_angleEquiv_neg, map_angleEquiv_zero, map_angleEquiv_add, map_angleEquiv_sub, map_angleEquiv_nsmul,
+    angleEquiv_pi, angleEquiv_mul_pi, angleEquiv_pi_div,
+    raySwap]))
+
+private lemma sub_eq_zero {a b : AddCircle (1 : ℝ)} : a - b = 0 → a = b := _root_.sub_eq_zero.mp
+
+private lemma coe_eq_zero {x : ℝ} : (∃ n : ℤ, n = x) → (x : AddCircle (1 : ℝ)) = 0 := by
+  rw [AddCircle.coe_eq_zero_iff 1]
+  simp
+
+macro "abel_mod_1" : tactic => `(tactic| (
+  refine sub_eq_zero ?_
+  try abel_nf -recursive
+  all_goals
+  simp -failIfUnchanged only [← QuotientAddGroup.mk_add, ← QuotientAddGroup.mk_zsmul]
+  all_goals
+  refine coe_eq_zero ?_
+  use ?_
+  norm_num
+  norm_cast
+  try exact rfl))
+
+macro "abel_angle" : tactic => `(tactic| angle_preprocess <;> abel_mod_1)
+
+example : (↑(π/2) : Angle) = ↑((5/2) * π) := by
+  abel_angle
+
+section
+variable (A B : ℂ) (h : A ≠ B)
+
+example  : ∠ A B = ∠ B A + ↑(9 * π) := by
+  abel_angle
+
+example  : ∠ B A = ∠ B A + ↑(10 * π) := by
+  abel_angle
+
+example  : (0 : Angle) =  ↑(10 * π) := by
+  abel_angle
+
+example  : ∠ A B = ∠ A B := by
+  abel_angle
+
+example  : ∠ A B = ∠ B A - π := by
+  abel_angle
+
+end
 
 end Angles
 
