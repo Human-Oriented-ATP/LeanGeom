bd14f63310ccd715a5dfbe4a4974a5bb4aa35e4d
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-05-24 23:05:06 +0100

    also support `↑(x * π / y)`

--- LeanGeom/Tactics.lean
+++ LeanGeom/Tactics.lean
@@ -60,7 +60,7 @@ private lemma angleEquiv_pi : AngleEquiv π = ↑(1/2 : Real) := by
   rw [AddCircle.equivAddCircle_apply_mk]
   simp [pi_ne_zero]
 
-private lemma angleEquiv_mul_pi (x : ℝ) : AngleEquiv ↑(x * π) = ↑(1/2 * x : Real) := by
+private lemma angleEquiv_mul_pi (x : ℝ) : AngleEquiv ↑(x * π) = ↑(x / 2 : Real) := by
   unfold AngleEquiv Angle.coe Angle
   rw [AddCircle.equivAddCircle_apply_mk, mul_assoc]
   simp [pi_ne_zero]
@@ -72,11 +72,17 @@ private lemma angleEquiv_pi_div (x : ℝ) : AngleEquiv ↑(π / x) = ↑((2 * x)
   simp [pi_ne_zero]
   ring_nf
 
+private lemma angleEquiv_mul_pi_div (x y : ℝ) : AngleEquiv ↑(x * π / y) = ↑(x / (2 * y) : Real) := by
+  unfold AngleEquiv Angle.coe Angle
+  rw [AddCircle.equivAddCircle_apply_mk, mul_div_assoc, mul_assoc, div_mul_eq_mul_div]
+  simp [pi_ne_zero]
+  ring_nf
+
 macro "angle_preprocess" : tactic => `(tactic| (
   refine angleEquiv_inj ?_
   simp only [
     ↓map_angleEquiv_neg, ↓map_angleEquiv_zero, ↓map_angleEquiv_add, ↓map_angleEquiv_sub, ↓map_angleEquiv_nsmul,
-    ↓angleEquiv_pi, ↓angleEquiv_mul_pi, ↓angleEquiv_pi_div,
+    ↓angleEquiv_pi, ↓angleEquiv_mul_pi, ↓angleEquiv_pi_div, ↓angleEquiv_mul_pi_div,
     ↓raySwap]))
 
 private lemma sub_eq_zero {a b : AddCircle (1 : ℝ)} : a - b = 0 → a = b := _root_.sub_eq_zero.mp
@@ -100,6 +106,9 @@ macro "abel_mod_1" : tactic => `(tactic| (
 macro "abel_angle" : tactic => `(tactic| angle_preprocess <;> abel_mod_1)
 
 example : (↑(π/2) : Angle) = ↑((5/2) * π) := by
+  abel_angle
+
+example : (↑(π/2) : Angle) = ↑(5 * π / 2) := by
   abel_angle
 
 variable (A B : ℂ) (h : A ≠ B)
